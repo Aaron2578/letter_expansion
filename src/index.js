9e07b217beb7682e06890763d45cba941860cb39
Aaron <115331438+Aaron2578@users.noreply.github.com>
2025-05-07 22:46:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-// import App from "./App";
 import FlashCard from "./FlashCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <FlashCard />
   </React.StrictMode>
 );
